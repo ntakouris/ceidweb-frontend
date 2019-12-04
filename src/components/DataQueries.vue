a8175fd4df9356b90d12788b4adef1a277fe96f9
Theodoros Zarkopafilis Ntakouris <zarkopafilis@gmail.com>
2019-12-04 17:09:12 +0200

    Add activity type filters

--- src/components/DataQueries.vue
+++ src/components/DataQueries.vue
@@ -1,7 +1,6 @@
 <template>
-  <v-col col="12">
+  <v-col col="12" class="ml-3">
     <v-switch v-model="dateRangeEnabled" :label="'Range Selection'"></v-switch>
-    <button @click="log(atTimeValue)">test</button>
     <v-row>
       <!-- At date picker -->
       <v-col col="3">
@@ -73,12 +72,7 @@
           offset-y
         >
           <template v-slot:activator="{ on }">
-            <v-text-field
-              label="Until time"
-              readonly
-              v-model="untilTimeValue"
-              v-on="on"
-            ></v-text-field>
+            <v-text-field label="Until time" readonly v-model="untilTimeValue" v-on="on"></v-text-field>
           </template>
           <v-time-picker
             v-if="untilTimeMenu"
@@ -89,10 +83,32 @@
         </v-menu>
       </v-col>
     </v-row>
+
+    <!-- Activity Types -->
+    <p>Filter Activity Types</p>
+    <v-overflow-btn v-for="(selection, index) in selectedActivityTypes" :key="index"
+      :items="availableActivityTypes"
+      @change="(e) => { selectedActivityTypes[index] = e }"
+    ></v-overflow-btn>
+
+    <v-row>
+        <v-btn class="mr-2" color="primary" @click="selectedActivityTypes.push('*')">Add activity type</v-btn>
+        <v-btn color="error" @click="selectedActivityTypes = ['*']">Clear activity filters</v-btn>
+    </v-row>
+    <v-spacer />
+    <v-row class="mt-2">
+        <v-btn class="mr-2" color="primary" @click="queryServer">Query Server</v-btn>
+        <v-btn color="accent" @click="exportCurrentData">Export current data</v-btn>
+    </v-row>
+
+    <!-- Visualizations -->
+
   </v-col>
 </template>
 
 <script>
+const availableActivityTypes = ['*', 'UNKNOWN', 'ON_FOOT', 'IN_VEHICLE']
+
 export default {
   name: "DataQueries",
   data() {
@@ -107,12 +123,17 @@ export default {
       atTimeMenu: false,
       untilTime: null,
       untilTimeValue: null,
-      untilTimeMenu: false
+      untilTimeMenu: false,
+      availableActivityTypes: availableActivityTypes,
+      selectedActivityTypes: [availableActivityTypes[0]]
     };
   },
   methods: {
-    log(x) {
-      console.log(x);
+    async queryServer(){
+        
+    },
+    exportCurrentData(){
+
     }
   }
 };
